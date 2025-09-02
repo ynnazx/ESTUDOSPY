2802b70e6618b760deeed3e5d99ab15f1e1feea3
ynna <cafeynna@gmail.com>
2025-09-02 19:14:11 -0300

    Add files via upload

--- LISTA02/df05.py
+++ LISTA02/df05.py
@@ -1,8 +1,6 @@
-while True:
-    num = int(input("Digite um número inteiro: "))
-    
-    if num % 2 == 0:
-        print("Número primo")
-        break
-    else:
-        print("O número que voce digitou não é primo")
+nome = "Lucas"
+idade = 22
+idade = str(idade)
+cidade = "Marechal"
+
+print("o " + nome + " tem " + idade + " anos de idade e mora na cidade de " + cidade + ".")
